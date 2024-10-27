3906ee9ef196517e4277255e55e35bc9540b30cd
S1GARETA <96253861+S1GARETA@users.noreply.github.com>
2024-10-27 22:08:49 +0500

    Update lesson.py

--- lesson.py
+++ lesson.py
@@ -1,1 +1,71 @@
+'''
+Списки, циклы for и while, словари, модуль random, функции
+'''
 
+# Списки
+
+# fruits = ["яблоко", "банан", "груша"]
+# print(fruits[0])
+
+# fruits[0] = 'апельсин'
+# print(fruits)
+
+# fruits.append('малина')
+# print(fruits)
+
+# fruits.remove('банан')
+# print(fruits)
+
+# Циклы for and while
+
+# for fruit in fruits:
+#     print(fruit)
+
+# count = 1
+# while count <= 5:
+#     print(count)
+#     count += 1
+    
+# while True: # Для создания меню
+#     count = int(input('Введите цифру: '))
+#     if count == 1:
+#         print('Цикл закончился')
+#         break
+#     else:
+#         print('Вечное цукиёми')
+# print('Новая команда')
+
+# Словари
+
+# people = {
+#     "Анна": 25,
+#     "Сергей": 24,
+#     "Дмитрий": 19,
+#     "Светлана": 24
+#     }
+
+# print(people["Сергей"])
+
+# people["Сергей"] = 16
+
+# print(people["Сергей"])
+
+
+# Модуль random
+# import random
+
+# number = random.randint(0, 2) # 0 or 1 or 2
+# print(number)
+
+# fruit = random.choice(fruits)
+# print(fruit)
+
+# Функции
+# def generate_hello(name):
+#     print(f'Hello, {name}')
+
+# def sum(a, b):
+#     result = a + b
+#     return result
+
+# print(sum(5, 6))
